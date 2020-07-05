c77a3dc48c42b3545f4b46b6bb8ecb79419a6ce5
surajm2021 <surajm2021@gmail.com>
2020-07-05 12:01:26 +0530

    virtualenv add

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\m2021\PycharmProjects\online_setu_api\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
